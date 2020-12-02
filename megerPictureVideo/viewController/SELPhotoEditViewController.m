b142c146ff124fbc8ab776be085a05550f807ff1
ljwcode <www.lovelyt@live.com>
2020-12-02 20:28:33 +0800

    视频与图片的合并。

--- megerPictureVideo/viewController/SELPhotoEditViewController.m
+++ megerPictureVideo/viewController/SELPhotoEditViewController.m
@@ -22,7 +22,7 @@ static NSString *SELThumbnailCollectionViewCellHeaderID = @"SELThumbnailCollecti
 static NSString *SELThumbnailCollectionViewCellFooterID = @"SELThumbnailCollectionViewCellFooterID";
 
 
-@interface SELPhotoEditViewController ()
+@interface SELPhotoEditViewController ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
 
 @property (weak, nonatomic) IBOutlet UIImageView *playerBgImgView;
 @property (weak, nonatomic) IBOutlet UIButton *PlayerBtn;
@@ -34,9 +34,12 @@ static NSString *SELThumbnailCollectionViewCellFooterID = @"SELThumbnailCollecti
 
 @property(nonatomic,strong)NSArray *bgImgArray;
 
-@property(nonatomic,assign)CGSize *itemSize;
+@property(nonatomic,assign)CGSize itemSize;
 
 @end
+
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
 
 @implementation SELPhotoEditViewController
 
@@ -47,17 +50,44 @@ static NSString *SELThumbnailCollectionViewCellFooterID = @"SELThumbnailCollecti
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     [self.videoFpsView registerNib:[UINib nibWithNibName:SELThumbnailCollectionViewCellID bundle:[NSBundle mainBundle]] forCellWithReuseIdentifier:SELThumbnailCollectionViewCellID];
     [self.videoFpsView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:SELThumbnailCollectionViewCellHeaderID];
     [self.videoFpsView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:SELThumbnailCollectionViewCellFooterID];
     
     [self.SELVideoBgView registerNib:[UINib nibWithNibName:SELBgCollectionViewCellID bundle:[NSBundle mainBundle]] forCellWithReuseIdentifier:SELBgCollectionViewCellID];
     [self.SELVideoBgView registerNib:[UINib nibWithNibName:SELBgMusicCollectionViewCellID bundle:[NSBundle mainBundle]] forCellWithReuseIdentifier:SELBgMusicCollectionViewCellID];
-    
+    self.SELVideoBgView.delegate = self;
+    self.SELVideoBgView.dataSource = self;
+    self.itemSize = CGSizeMake(kScreenWidth/4.0, kScreenWidth/4.0);
+    self.SELVideoBgView.layer.borderColor = [UIColor redColor].CGColor;
+    self.SELVideoBgView.layer.borderWidth = 2.f;
     // Do any additional setup after loading the view from its nib.
 }
 
+#pragma mark ------ UICollectionView delegate && dataSource
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return self.bgImgArray.count;
+}
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    UICollectionViewCell *resultCell = nil;
+    if(collectionView == self.SELVideoBgView){
+        SELBgCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:SELBgCollectionViewCellID forIndexPath:indexPath];
+        [cell.SELBgImgView setImage:[UIImage imageNamed:self.bgImgArray[indexPath.row]]];
+        resultCell = cell;
+    }
+    
+    
+    return resultCell;
+}
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    return self.itemSize;
+}
+
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
 
 #pragma mark ------ 响应事件
 
